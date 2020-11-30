3dd360a54f3471d44d1e6fbb745334305cec0b04
Javier Rozas <javier.rozasacurio@gmail.com>
2020-11-30 18:54:35 -0500

    Inicio

--- PapuEngine/main.cpp
+++ PapuEngine/main.cpp
@@ -13,8 +13,22 @@ int main(int argc,char** argv) {
 
 	PlaySound(TEXT("song.wav"), NULL, SND_FILENAME | SND_ASYNC);
 
-	char start = 'a';
+	cout << "\033[1;31m**********************************************\033[0m\n" << endl;
+	cout << "\033[1;31m****************DOOM ETERNAL******************\033[0m\n" << endl;
+	cout << "\033[1;31m**********************************************\033[0m\n";
 
+	cout << " " << endl;
+	cout << "******Fundamentos de la Teoria de Juegos******" << endl;
+	cout << " " << endl;
+	cout << "-Geral Esteen Castillo Arredondo U201716913" << endl;
+	cout << "-Rafael Andrés Andrade Rodríguez U201812443" << endl;
+	cout << "-Javier Arturo Rozas Acurio U201711814" << endl;
+	cout << " " << endl;
+
+	cout << "\033[1;31m**********************************************\033[0m\n" << endl;
+	cout << " " << endl;
+
+	char start = 'a';
 	cout << "Presione 's' para empezar: ";
 	cin >> start;
 
